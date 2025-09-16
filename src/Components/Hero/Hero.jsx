ad308401cffa387a4c7b07c897e3d4f283e78264
ayushkumarpandey123 <ayushpandey77737@gmail.com>
2025-09-16 10:39:28 +0530

    Inital Commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -84,7 +84,7 @@ const Hero = () => {
           </button>
 
           <button className="btn announcement-btn" onClick={handleAnnouncementClick}>
-            Important Dates <span className="arrow">📅</span>
+            Important Dates <span className="arrow">→</span>
           </button>
         </div>
       </div>
