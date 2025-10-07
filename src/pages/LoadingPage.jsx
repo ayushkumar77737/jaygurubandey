99bd29b0c2ca9a0031a7cd966b3631d2a32811c9
ayushkumarpandey123 <ayushpandey77737@gmail.com>
2025-10-07 11:42:48 +0530

    Inital Commit

--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import "./LoadingPage.css";
-import circle from "../assets/circle.png";
+import circle from "../assets/circle.png"; // your image
 
 const LoadingPage = () => {
   return (
     <div className="loading-page">
-      {/* Glowing Rings */}
-      <div className="ring"></div>
-      <div className="ring"></div>
-      <div className="ring"></div>
+      {/* Particle overlay */}
+      <div className="particles"></div>
 
-      {/* Center Circle Image */}
-      <img src={circle} alt="Loading..." className="loading-circle" />
+      {/* Glowing Orb around the image */}
+      <div className="orb-wrapper">
+        <img src={circle} alt="Loading..." className="loading-circle" />
+      </div>
+
+      {/* Animated Text */}
+      <div className="loading-text">
+        <h1 className="main-text">🌸 Surrender to Guruji’s Grace 🌸</h1>
+        <p className="sub-text">
+          In surrender, there is peace; in peace, there is the Divine.
+        </p>
+        <div className="dots">...</div>
+      </div>
     </div>
   );
 };
