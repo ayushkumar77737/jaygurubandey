b8a116ad06889b17e10457d48b5261615f97951d
ayushkumarpandey123 <ayushpandey77737@gmail.com>
2026-01-06 22:35:05 +0530

    Inital Commit

--- src/pages/Maintenance.jsx
+++ src/pages/Maintenance.jsx
@@ -9,7 +9,7 @@ export default function Maintenance() {
         The website will resume shortly.
       </p>
       <p className="resume-text">
-        Please visit again after <strong>12:20 PM</strong>
+        Please visit again after <strong>05:01 AM</strong>
       </p>
     </div>
   );
