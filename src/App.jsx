2f3d3270c123adcaf324addf0814cef986ffe0c8
ayushkumarpandey123 <ayushpandey77737@gmail.com>
2026-01-08 14:33:02 +0530

    Inital Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
-import { Outlet } from 'react-router-dom'
+import {Outlet} from 'react-router-dom'
 import Footer from './pages/Footer'
 import CookieConsent from './Components/Cookies/CookieConsent'
 import Maintenance from './pages/Maintenance'
-import { isMaintenanceTime } from './utils/maintenance'
+import {isMaintenanceTime} from './utils/maintenance'
+import ScrollToTop from './Components/ScrollToTop/ScrollToTop'
 
-const App = () => {
-  if (isMaintenanceTime()) {
-    return <Maintenance />
+const App=()=>{
+  if(isMaintenanceTime()){
+    return <Maintenance/>
   }
-  return (
+
+  return(
     <>
-      <Navbar />
-      <main>
-        <Outlet /> {/* Renders the nested route like Home, About, etc. */}
-      </main>
-      <Footer />
-      <CookieConsent />
+      {/* 🔥 Single scroll container */}
+      <div className="app-scroll">
+        <Navbar/>
+
+        <main>
+          <Outlet/>
+        </main>
+
+        <Footer/>
+      </div>
+
+      <CookieConsent/>
+      <ScrollToTop/>
     </>
   )
 }
