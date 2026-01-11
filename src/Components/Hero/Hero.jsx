8176c587ca70544502ccb6cfc3cd066c70ea9457
ayushkumarpandey123 <ayushpandey77737@gmail.com>
2026-01-11 12:27:11 +0530

    Inital Commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -14,7 +14,7 @@ import photo1 from "../../assets/photo1.jpg";
 import photo6 from "../../assets/photo6.webp";
 import photo7 from "../../assets/photo7.webp";
 import photo8 from "../../assets/photo8.webp";
-import photo10 from "../../assets/photo10.jpg";
+import photo10 from "../../assets/photo10.webp";
 import photo19 from "../../assets/photo19.jpg";
 import pic from "../../assets/pic.jpeg";
 import {
