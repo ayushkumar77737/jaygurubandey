e3b2de75c030d06904cf95f827a12404d358328d
ayushkumarpandey123 <ayushpandey77737@gmail.com>
2026-01-14 23:41:45 +0530

    Inital Commit

--- src/pages/SatguruChalisa.jsx
+++ src/pages/SatguruChalisa.jsx
@@ -25,71 +25,44 @@ const SatguruChalisa = () => {
         {/* Right: Text */}
         <div className="arti-text-box">
           {page === 1 && (
-            <p style={{ whiteSpace: "pre-wrap", textAlign: "center" }}>
-              <b>(1)</b> <br />
-              Pooja Satguru Charan Ka, Karen Nat Jo Dhyanan | <br />
-              Tare Kripa Se, Jai Guru Bande, Sahaj Usi Ka Paran | <br />
-              Shraddha Bhakt Bada Re Manav, Man Karo Majbootan | <br />
-              Jai Guru Bande Satguru Ko, Ishwar Jano Dootan | <br /><br />
+            <p className="arti-text">
+              <b>Doha</b> <br />
+              Poojaa Satguru charan kaa, kare nitya jo dhyaan | <br />
+              Tere kripaa se jai Gurubande, sahaj usii kaa praan || <br /><br />
 
-              Satguru Ram Ratan Run Kani, Daata Deen Daal Udani | <br />
-              Saheb Satguru Jeev Ubaraata, Aaye Jagat Me Bhay Bhakataan | <br />
-              Satguru Bhakt Bane Jo Koi, Baal Nawa Ka Uska Hoi | <br />
-              Rakhe Jo Satguru Ko Aage, Bhoot Bhav Sab Dekhat Bhaage | <br /><br />
+              <b>Chalisa</b> <br />
+              Jai jai jai Satguru jii devaa, karen jo maanav bhakti sevaa | <br />
+              Sukh shaanti ghar satsaraṇ aave, janam maran bhav rog nasaave || <br />
+              Mile bhaagya se Satguru daataa, raakhe jiiv se bhakti naataa | <br />
+              Satguru naam jo bhitar gaave, adbhut tirath kaa phal paave || <br /><br />
 
-              Jeh Ghar Satguru Karen Nivasa, Det Prem Atal Vishwasa | <br />
-              Satguru Naam Athaah Tasha, Kare Hriday Mein Param Prakasha | <br />
-              Satguru Bachan Hid Jo Rakha, Man Me Dhar Me Amrit Chakha | <br />
-              Rakhe Ek Jo Satguru Aasa, Amar Lok Ghar Bhavi Vaasa | <br />
+              Satguru dukh hare sukhdaai, janam janam kaa paap nasaai | <br />
+              Satguru saran jiivan jo aave, Prabhu kaa pyaaraa so ban jaave || <br />
+              Satguru Raam ratan dhan khaani, daataa diin dayaalu daanii | <br />
+              Saahib Satguru jiiv vidhaataa, aaye jagat bhaye vikhyaataa ||
             </p>
           )}
 
           {page === 2 && (
-            <p style={{ whiteSpace: "pre-wrap", textAlign: "center" }}>
-              <b>(2)</b> <br />
-              Satguru Vaid Hai Bachan Dawaai, Saara Manas Rog Hataai | <br />
-              Satguru Mahima Jaye Na Barni, Jeevan Ko Taare Mangal Karni | <br />
-              Satsang Teer Jo Maarat Gota, Man Me Lachat Chadar Rota | <br />
-              Satguru Ek Hai Samarth Swami, Bhedi Ghat Ka Ant Aatmaami | <br /><br />
+            <p className="arti-text">
+              Satguru bhagat bane jo koi, baal na baankaa uskaa hoi | <br />
+              Raakhe jo Satguru ko aage, bhoot bhram sab dekhat bhaage || <br />
+              Jehi ghar Satguru kare nivaasaa, dete prem atal vishwaasaa | <br />
+              Satguru naam akhand akaashaa, kare hriday mein param prakaashaa || <br /><br />
 
-              Suru, Naru, Manu Sansar Mein Koi, Bin Satguru Bhav Paar Na Hoi | <br />
-              Jo Satguru Pad Dhyan Lagai, Ghat Mein Aatma Jaan Samai | <br />
-              Bin Satguru Ka Jeevan Garua Bhaari, Mile Jo Manav Saguara | <br />
-              Satguru Sharir Charan Jo Paaya, Param Anand Hriday Mein Aaya | <br /><br />
+              Raakhe ek jo Satguru aashaa, amarlok ghar paave vaasaa | <br />
+              Satguru vaidya hai vachan davaai, saaraa maanas rog hataai || <br />
+              Satguru mahimaa jaay na varni, jiiv ko taare mangal karni | <br />
+              Satsang tirath jo maarat gotaa, man mailaa chit chaadar dhotaa || <br />
+              Satguru ek hai samarth swaami, bhedi ghat kaa antaryaami || <br /><br />
 
-              Satguru Kripa Hota Hai Jahan Par, Ram Kripa Karte Hai Tahan Par | <br />
-              Satguru Ne Sab Dev Samaaya, Khol Bhkuti Dwaar Jalaya | <br />
-              Satguru Naam Se Rati Maaya, Samumrat Daas Param Sukh Paaya | <br />
-              Jo Satguru Ko Eesh Banaaya, Kaal Nahin Achar Kar Jaaya | <br />
-            </p>
-          )}
-
-          {page === 3 && (
-            <p style={{ whiteSpace: "pre-wrap", textAlign: "center" }}>
-              <b>(3)</b> <br />
-              Sakal Jeev Eeshwar Ka Ansha, Satguru Sangat Kare Sab Asha | <br />
-              Ansh Deh Mein Rahte Bandi, Hansan Karat Bhajan Aanandan | <br />
-              Jo Satguru Ka Kane Bhakt, So Paave Sneh Ki Shakti Detan | <br />
-              Satguru Prem Ka Paani, Nikal Mukh Se Murur Hi Vaarin | <br /><br />
-
-              Guru Satguru Ka Babal Lagaave, Ghat Ka Bhed Vahi Sab Paave | <br />
-              Satguru Saar Shabad Ke Daani, Mukti Moksh Barse Sab Faani | <br /><br />
-
-              Satguru Shabad Akhand Apaara, Saakha Sab Daal Ek Sahara | <br />
-              Deen Daal Se Bheetar Parkha, Kasar Hua Shwas Ka Charkha | <br />
-              Amar Lok Satguru Ka Aasan, Baithe Vahin Laga Simhaasan | <br />
-              Roop Dekhi Anant Sundar Dekha, Daas Adhyayan Samundran | <br /><br />
-
-              Hriday Sheesh Bana Jo Daani, Mahima Satguru Sab Ka Jaani | <br />
-              Aagam Alag Se Paar Ram, Dekhat Sant Hai Naam Naman | <br />
-              Jo Satguru Ko Nishchid Dhaave, Naam Amar Jag Mein Ho Jaave | <br />
-              Jo Satguru Ka Padhe Chalisa, Jai Guru Bande Det Hai Aseesa | <br /><br />
-
-              Jai Guru Bande Det Hai Aseesa, Jo Satguru Ka Padha Chalisa | <br />
-              Ghat Mein Prem Ka Paath Khole, Kripa Se Jai Guru Bande Jod Chetan Ka Gharan | <br />
+              Sur nar muni sansaar mein koi, bin Satguru bhav paar na hoi | <br />
+              Jo Satguru pad dhyaan lagaai, ghat mein aatam gyaan samaai || <br />
+              Bin Satguru kaa jiiv hai nirguraa, Hari mile jo maanav saguraa ||
             </p>
           )}
         </div>
+
       </div>
 
       {/* Pagination */}
@@ -97,8 +70,8 @@ const SatguruChalisa = () => {
         <button onClick={handlePrev} disabled={page === 1}>
           ⬅️ Previous
         </button>
-        <span>Page {page} of 3</span>
-        <button onClick={handleNext} disabled={page === 3}>
+        <span>Page {page} of 2</span>
+        <button onClick={handleNext} disabled={page === 2}>
           Next ➡️
         </button>
       </div>
