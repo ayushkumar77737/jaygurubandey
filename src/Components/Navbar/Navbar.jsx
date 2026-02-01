cebf17af822ef57329249d649372ff554eee4d4f
ayushkumarpandey123 <ayushpandey77737@gmail.com>
2026-02-01 10:42:07 +0530

    Inital Commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -52,6 +52,18 @@ const Navbar = () => {
     }
   }, [openDropdown])
 
+  useEffect(() => {
+  if (mobileMenu) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "";
+  }
+
+  return () => {
+    document.body.style.overflow = "";
+  };
+}, [mobileMenu]);
+
   const handleLogout = async () => {
     try {
       await signOut(auth);
