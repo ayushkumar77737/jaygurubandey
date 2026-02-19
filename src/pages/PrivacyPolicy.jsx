a36aa8b6e6128e38ca9b1c0d77c03004a948a4f6
ayushkumarpandey123 <ayushpandey77737@gmail.com>
2026-02-19 10:11:10 +0530

    Inital Commit

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -39,7 +39,7 @@ const PrivacyPolicy = () => {
           <h2>Contact Us</h2>
           <p>
             If you have any questions about this Privacy Policy, please contact us
-            at <span className="highlight">jaigurubande15@gmail.com</span>.
+            at <span className="highlight">jaigurubande15@gmail.com</span>
           </p>
         </div>
       </div>
