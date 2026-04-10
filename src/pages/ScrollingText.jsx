94222b39f7d42db3d525a83a9fb0c481899d3a13
ayushkumarpandey123 <ayushpandey77737@gmail.com>
2026-04-10 09:36:21 +0530

    Inital Commit

--- src/pages/ScrollingText.jsx
+++ src/pages/ScrollingText.jsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { useTranslation } from "react-i18next";   // ✅ NEW
 import "./ScrollingText.css";
 
-const MARQUEE_CONTENT = (
-  <>
-    ✿ Jai Gurubande Saheb Sabka &nbsp;🙏&nbsp; &nbsp;&nbsp;&nbsp;
-    ✿ Jai Gurubande Saheb Sabka &nbsp;🙏&nbsp; &nbsp;&nbsp;&nbsp;
-    ✿ Jai Gurubande Saheb Sabka &nbsp;🙏&nbsp; &nbsp;&nbsp;&nbsp;
-    ✿ Jai Gurubande Saheb Sabka &nbsp;🙏&nbsp; &nbsp;&nbsp;&nbsp;
-    ✿ Jai Gurubande Saheb Sabka &nbsp;🙏&nbsp; &nbsp;&nbsp;&nbsp;
-  </>
-);
-
 const ScrollingText = () => {
+  const { t } = useTranslation();   // ✅ NEW
+  const text = t("scrolling.text");
+
+  const content = (
+    <>
+      {text} &nbsp;&nbsp;&nbsp;
+      {text} &nbsp;&nbsp;&nbsp;
+      {text} &nbsp;&nbsp;&nbsp;
+      {text} &nbsp;&nbsp;&nbsp;
+      {text} &nbsp;&nbsp;&nbsp;
+    </>
+  );
+
   return (
     <div className="sctxt__container">
       <div className="sctxt__glow-strip" />
       <div className="sctxt__track">
         <div className="sctxt__belt">
-          <span className="sctxt__segment">{MARQUEE_CONTENT}</span>
-          <span className="sctxt__segment" aria-hidden="true">{MARQUEE_CONTENT}</span>
+          <span className="sctxt__segment">{content}</span>
+          <span className="sctxt__segment" aria-hidden="true">{content}</span>
         </div>
       </div>
     </div>
